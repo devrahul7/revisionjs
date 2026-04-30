bc32483bab0ea9e54b031f4db1ce1b773c211bba
Rahul Shah <rahul6xdev@gmail.com>
2026-04-30 10:02:38 +0545

    postman

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from "express";
 import { dataset } from "../models/person.model";
+import { ApiResponseHelper } from "../utils/api.helper.utils";
 
 export class PersonController {
     async getAllPersons(req: Request, res: Response) {
         // path function
-        return res.json(dataset);
+        // return res.json(dataset);
+
+
+        try{
+            const someVar: any = {};
+            someVar.ref.add("test"); //error
+        
+
+        return ApiResponseHelper.success(
+            res,dataset, "Person fetched succesfully",200
+        );
+
+    }catch (err: Error | any){
+        return ApiResponseHelper.error(
+            res, "Failed to fetch persons",500
+        );
     }
 }
+    //Api consistency
+}
+
